f1f214726d073d3cbf1d83adb73acf26d639fdb3
Chetan Rathore <chetan.rathore.dev@gmail.com>
2017-04-05 11:31:53 +0530

    Create dashboard tab bar.

---
+++ DealerDNA/DealerDNA/DashboardVC.swift
@@ -0,0 +1,24 @@
+//
+//  DashboardVC.swift
+//  DealerDNA
+//
+//  Created by LaNet on 4/5/17.
+//  Copyright © 2017 developer93. All rights reserved.
+//
+
+import UIKit
+
+class DashboardVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
